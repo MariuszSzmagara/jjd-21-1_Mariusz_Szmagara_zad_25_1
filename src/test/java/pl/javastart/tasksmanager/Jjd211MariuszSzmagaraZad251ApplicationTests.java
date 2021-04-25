f17c2470e4a51dafba1699eaa6c577ae849e0bf4
MariuszSzmagara <mariusz.szmagara@gmail.com>
2021-04-25 10:37:33 +0200

    Initial commit

---
+++ src/test/java/pl/javastart/tasksmanager/Jjd211MariuszSzmagaraZad251ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.javastart.tasksmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jjd211MariuszSzmagaraZad251ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
